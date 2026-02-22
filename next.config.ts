ba6d34eb25f9394ad923bd98a997c915119ca1f8
Code1811 <31153711+Code1811@users.noreply.github.com>
2026-02-22 12:25:22 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -22,7 +22,6 @@ const standaloneConfig: NextConfig = {
 const assetPrefix = process.env.NEXT_PUBLIC_ASSET_PREFIX;
 
 const nextConfig: NextConfig = {
-  staticPageGenerationTimeout: 500,
   ...(isStandaloneMode ? standaloneConfig : {}),
   assetPrefix,
   compiler: {
